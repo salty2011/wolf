b92cb09dcbae19be0764ac7027642eb264776801
StimzRx <103589496+StimzRx@users.noreply.github.com>
2024-08-20 01:01:32 -0500

    Update overlayfs-entrypoint.sh - explainations

--- overlayfs-entrypoint.sh
+++ overlayfs-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-## VARIABLES ##
-
-OverlayGuestMountPoint=.steam/debian-installation/steamapps/
-
-###############
-
-
-
+# Make sure the overlayfs directorys and targeted overlay directory exist
 mkdir -p /overlayfs/user/{upper,work}
-mount -t overlay overlay -o lowerdir=/overlayfs/steam-ro/,upperdir=/overlayfs/user/upper/,workdir=/overlayfs/user/work/ /home/retro/$OverlayGuestMountPoint
+mkdir -p /home/retro/.steam/debian-installation/steamapps
+
+# Create the overlay using:
+# 'lowerdir' is the readonly steamapps directory we defined in /etc/wolf/cfg/config.toml
+# 'upperdir' is the persistent "read & write" directory
+# 'workdir' seems to be like a cache for files that are in a in-between state? Its required reguardless and needs to be not a subfolder of 'upperdir'
+# finally, the final arg '/home/retro/.steam/debian-installation/steamapps' is the targeted overlay directory we want the overlay to exist at
+mount -t overlay overlay -o lowerdir=/overlayfs/steam-ro/,upperdir=/overlayfs/user/upper/,workdir=/overlayfs/user/work/ /home/retro/.steam/debian-installation/steamapps
 
 source /entrypoint.sh
